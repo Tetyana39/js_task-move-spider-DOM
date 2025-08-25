a223b4778f0f8430d8e039a9f762fb662fc26492
Tetiana Susol <tetyanamyeskalo@gmail.com>
2025-08-25 15:38:12 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = wall.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  const min = 0;
+  const maxX = wall.clientWidth - spider.clientWidth;
+  const maxY = wall.clientHeight - spider.clientHeight;
+
+  let x = e.offsetX - spider.clientWidth / 2;
+  let y = e.offsetY - spider.clientHeight / 2;
+
+  if (x < min) {
+    x = min;
+  }
+
+  if (y < min) {
+    y = min;
+  }
+
+  if (x > maxX) {
+    x = maxX;
+  }
+
+  if (y > maxY) {
+    y = maxY;
+  }
+
+  spider.style.top = `${y}px`;
+  spider.style.left = `${x}px`;
 });
